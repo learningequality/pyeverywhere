3fa1c71f0afa31a823b532b3821d339752f46a08
Jamie Alexandre <jamalex@gmail.com>
2021-12-11 19:50:00 -0800

    Ensure licenses are accepted.

--- native/android/init.sh
+++ native/android/init.sh
@@ -39,6 +39,7 @@ then
     yes y | ./sdkmanager "platforms;android-$ANDROIDAPI" --sdk_root=$SDK_ROOT
     yes y | ./sdkmanager "system-images;android-$ANDROIDAPI;default;x86_64" --sdk_root=$SDK_ROOT
     yes y | ./sdkmanager "build-tools;$ANDROIDBUILDTOOLSVER" --sdk_root=$SDK_ROOT
+    echo "Accepting all licenses"
     yes | ./sdkmanager --licenses --sdk_root=$SDK_ROOT
     cd ../../..
 fi
